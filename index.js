3904507f7106d14a589f6b4490d718e261b67101
Luke <luke.towell@hotmail.com>
2019-08-15 22:02:34 +0100

    Initial Commit
    Initial ommit of basic node application with some redis functionality

---
+++ index.js
@@ -0,0 +1,17 @@
+const redis = require('redis');
+const express = require('express');
+
+const app = express();
+const client = redis.createClient();
+client.set('visits', 0)
+
+app.get('/', (req,res) => {
+    client.get('visits', (err, visits) => {
+        res.send("number of visits in: " + visits);
+        client.set('visits', parseInt(visits) + 1);
+    });
+});
+
+app.listen(3000, () =>{
+    console.log("listening in container on port: 3000");
+});
